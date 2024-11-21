8c2c914fad2e545f1b32c1e86e34e0fe8f28cb30
veurret <joshuarusli65@gmail.com>
2024-11-21 16:36:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function changeText() {
+    var replaceText = document.    
+    getElementsByClassName("mini-text");
+    replaceText[0].innerHTML = "scroll down";
+ 
+    document.getElementById("icon-up").style.display = "none";
+ 
+    document.getElementById("icon-down").style.display = "block";
+ 
+ }
+
+ var btnSubmit = document.querySelector('.submit');
+var modal = document.querySelector('.modal-container');
+
+if (btnSubmit) {
+btnSubmit.addEventListener('click', function () {
+modal.classList.add('show');
+
+
+const next = document.querySelector('.next');
+next.setAttribute('value', "index.html");
+});
+}
