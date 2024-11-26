a5a986df0718ee40c634e64d6c5e9236d56d4dea
veurret <joshuarusli65@gmail.com>
2024-11-26 16:46:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+function changeText() {
+    var replaceText = document.    
+    getElementsByClassName("mini-text");
+    replaceText[0].innerHTML = "scroll down";
+ 
+    document.getElementById("icon-up").style.display = "none";
+ 
+    document.getElementById("icon-down").style.display = "block";
+ 
+ }
+
+ var btnSubmit = document.querySelector('.submit');
+var modal = document.querySelector('.modal-container');
+
+if (btnSubmit) {
+btnSubmit.addEventListener('click', function () {
+modal.classList.add('show');
+
+
+const next = document.querySelector('.next');
+next.setAttribute('value', index.html);
+});
+}
+
+var slides = document.querySelectorAll(".slide");
+var buttons = document.querySelectorAll(".slider-btn");
+let currentSlide = 1;
+
+//Manual images slider
+var manualNav = function(manual) {
+    slides.forEach(function (slide) {
+        slide.classList.remove("active");
+
+        buttons.forEach((btn) => {
+            btn.classList.remove("active");
+        });
+    });
+
+    slides[manual].classList.add("active");
+    buttons[manual].classList.add("active");
+}
+
+buttons.forEach(function(btn, i) {
+    btn.addEventListener("click", function() {
+        manualNav(i);
+        currentSlide = i;
+    })
+})
+
+var repeat = function(activeClass) {
+  let active = document.
+               getElementsByClassName("active");
+
+  let i = 1;
+  var repeater = function() {
+    setTimeout(function() {
+       [...active].forEach(function(activeSlide) {
+          activeSlide.classList.remove("active");
+       });
+
+       slides[i].classList.add("active");
+       buttons[i].classList.add("active");
+       i++
+
+       if(slides.length == i) {
+         i = 0;
+       }
+       if(i >= slides.length) {
+         return;
+       }
+       repeater(); 
+     }, 5000);
+  } 
+  repeater();
+}
+repeat();
